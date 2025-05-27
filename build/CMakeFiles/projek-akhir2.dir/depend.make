fb79931307bd8ade2b133e3b0c88aa46f267b30f
InfinityNichto <u5170201@gmail.com>
2025-05-27 21:48:22 +0700

    untrack ignored files

--- build/CMakeFiles/projek-akhir2.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for projek-akhir2.
-# This may be replaced when dependencies are built.
--- build/make-n-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-make && ./projek-akhir2
-
-counter=$(<../build_counter)
-((counter++))
-printf "%s" "$counter" > ../build_counter
-echo -n "build counter: "
-cat ../build_counter
-echo
